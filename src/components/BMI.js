da3c273f64264ba67cd1725a259ed4834eaeb9b5
anandhakrishnan-ar <anandhank77@gmail.com>
2021-11-17 15:26:22 +0530

    calculation completed

--- src/components/BMI.js
+++ src/components/BMI.js
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import BmiCalculator from './BmiCalculator'
 
 const BMI = () => {
+    const [bmiValue, setBmiValue] = useState(0);
+    const [result, setResult] = useState('')
+    useEffect(() => {
+        if (bmiValue > 0 && bmiValue < 18.5) {
+            setResult('Underweight')
+        }
+        else if (bmiValue >= 18.5 && bmiValue < 23) {
+            setResult('Normal')
+        }
+        else if (bmiValue >= 23 && bmiValue < 25) {
+            setResult('Overweight')
+        }
+        else if (bmiValue >= 25 && bmiValue < 30) {
+            setResult('Pre-Obese')
+        }
+        else if (bmiValue >= 30) {
+            setResult('Obese')
+        }
+        else {
+            setResult('')
+        }
+
+    }, [bmiValue])
     return (
-        <div className="container">
-            <span>This is a test app</span>
-            <BmiCalculator/>
+        <div className="calculator-wrapper">
+            <div className="app-header"><h3>BMI CALCULATOR</h3></div>
+            <div className="app-header">
+                Body Mass Index(BMI) : {bmiValue}
+            </div>
+            <div className="app-result font-weight-light">
+                {result}
+            </div>
+            <BmiCalculator getBmiValue={setBmiValue} />
         </div>
     )
 }
