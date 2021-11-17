fe95816c6b6261b1138331aafaa81f53efc8d921
anandhakrishnan-ar <anandhank77@gmail.com>
2021-11-17 17:04:44 +0530

    Updates

---
+++ precache-manifest.2b44d62ffafb7654a88ed1a2106f5fb5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9254403943aec9a4c1410cdc75a3cc47",
+    "url": "/bmi-calculator/index.html"
+  },
+  {
+    "revision": "0c9bb202a9b532e684aa",
+    "url": "/bmi-calculator/static/css/2.fa43bc9e.chunk.css"
+  },
+  {
+    "revision": "6240a59c067f14530fdf",
+    "url": "/bmi-calculator/static/css/main.97ab8b6e.chunk.css"
+  },
+  {
+    "revision": "0c9bb202a9b532e684aa",
+    "url": "/bmi-calculator/static/js/2.a820f0be.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/bmi-calculator/static/js/2.a820f0be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3d15cc9bfc8bb3c3621",
+    "url": "/bmi-calculator/static/js/3.c4168188.chunk.js"
+  },
+  {
+    "revision": "6240a59c067f14530fdf",
+    "url": "/bmi-calculator/static/js/main.047e3af1.chunk.js"
+  },
+  {
+    "revision": "cf435f8686fe3af405e2",
+    "url": "/bmi-calculator/static/js/runtime-main.865b3168.js"
+  }
+]);
